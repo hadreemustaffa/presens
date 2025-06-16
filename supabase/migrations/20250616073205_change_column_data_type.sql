b1b4e6bca2f5f1f1ed6316e6d65fc93af1563a22
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-06-16 23:12:39 +0800

    add db migration file

---
+++ supabase/migrations/20250616073205_change_column_data_type.sql
@@ -0,0 +1,11 @@
+alter table attendance_records
+alter column clock_in set data type time;
+
+alter table attendance_records
+alter column clock_out set data type time;
+
+alter table attendance_records
+alter column lunch_out set data type time;
+
+alter table attendance_records
+alter column lunch_in set data type time;
